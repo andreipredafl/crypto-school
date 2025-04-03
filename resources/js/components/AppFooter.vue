982dec3af2b47fb4284dc687e50ce61a015656f2
andreipreda <andreipredafl@gmail.com>
2025-04-03 12:06:41 +0300

    feat(layout): add footer - totally forgot about it 😅

---
+++ resources/js/components/AppFooter.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+	<footer class="border-t border-sidebar-border/70 bg-white dark:bg-neutral-900">
+		<div
+			class="mx-auto max-w-7xl px-4 py-6 text-sm text-neutral-500 flex flex-col md:flex-row items-center justify-between"
+		>
+			<span> © {{ new Date().getFullYear() }} Andrei Preda. All rights reserved 🚀</span>
+		</div>
+	</footer>
+</template>
